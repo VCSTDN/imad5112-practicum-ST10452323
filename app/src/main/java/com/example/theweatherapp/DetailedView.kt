97fa5bcd4197d92704ad0e1f6b0a0b17c3905b3a
ST10452323 <ST10452323@vcconnect.edu.za>
2024-06-10 14:54:36 +0200

    final commit

--- app/src/main/java/com/example/theweatherapp/DetailedView.kt
+++ app/src/main/java/com/example/theweatherapp/DetailedView.kt
@@ -1,11 +1,22 @@
 package com.example.theweatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetailedView : AppCompatActivity() {
+
+    private lateinit var buttonMainScreen: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailed_view)
+
+
+        buttonMainScreen.setOnClickListener {
+            val intent = Intent(this, MainScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
