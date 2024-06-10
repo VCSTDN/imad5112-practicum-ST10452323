6268bea5a54a6e57762b436477e8c9923016def6
ST10452323 <ST10452323@vcconnect.edu.za>
2024-06-10 11:50:21 +0200

    first commit

--- app/src/main/java/com/example/theweatherapp/MainScreen.kt
+++ app/src/main/java/com/example/theweatherapp/MainScreen.kt
@@ -1,11 +1,32 @@
 package com.example.theweatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainScreen : AppCompatActivity() {
+
+    private lateinit var buttonDetailed: Button
+    private lateinit var buttonClear: Button
+    private lateinit var buttonExit2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+
+        buttonDetailed= findViewById(R.id.buttonDetailed)
+        buttonClear= findViewById(R.id.buttonClear)
+        buttonExit2= findViewById(R.id.buttonExit2)
+
+        buttonDetailed.setOnClickListener {
+            val intent = Intent(this, DetailedView::class.java)
+            startActivity(intent)
+        }
+
+        buttonExit2.setOnClickListener {
+            finish()
+        }
+
+
     }
 }
